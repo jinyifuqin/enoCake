6a1c821a40b5981808e61ee9abdaafe8dde4e7b1
yangyh <jinyifuqin@163.com>
2018-06-03 19:03:30 +0800

    comment

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'           => 'mysql',
     // 服务器地址
-    'hostname'       => '47.94.10.129',
+    'hostname'       => '192.168.1.189',
     // 数据库名
     'database'       => 'eno',
     // 用户名
